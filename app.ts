7da48ee9d53a3af7a495a4ebf40b034f6973c221
Nayana333 <nrnayana1@gmail.com>
2024-09-27 17:03:15 +0530

    hosted5

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.ORIGINFRONTEND,
+    origin: "*",
     credentials: true
 }));
 app.use('/files', express.static('files'));
